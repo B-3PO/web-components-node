38d60fc446b9d7711e0c646ffd48ce067a3fee19
Ben Rubin <ben@benjaminrubin.com>
2018-11-30 10:21:53 -0600

    fix example

--- example/pages/interactive.js
+++ example/pages/interactive.js
@@ -17,12 +17,8 @@ const page = customElements.defineWithRender('home-page', class extends HTMLElem
   }
 
   async connectedCallback() {
-    const [ listData, stateData ] = await Promise.all([
-      axios.get('/api/list'),
-      axios.get('/api/states')
-    ]);
-    this.list = listData.list;
-    this.stateData = listData.states;
+    const { data } = await axios.get('/api/states');
+    this.states = data.states;
     this.render();
   }
 
@@ -36,14 +32,14 @@ const page = customElements.defineWithRender('home-page', class extends HTMLElem
             <option value="" disabled ${this.selectedState === null ? 'selected' : ''}>State...</option>
             ${this.states.map(s => html`
               <option value="${s.name}" ${this.selectedState === s.name ? 'selected' : ''}>${s.name}</option>
-            `).join('/n')}
+            `).join('\n')}
           </select>
 
           <select onchange="$homePage.citySelectChange(this.value)">
             <option value="" disabled ${this.selectedCity === null ? 'selected' : ''}>City...</option>
-            ${this.states.map(c => html`
+            ${this.cities.map(c => html`
               <option value="${c.name}" ${this.selectedCity === c.name ? 'selected' : ''}>${c.name}</option>
-            `).join('/n')}
+            `).join('\n')}
           </select>
         </div>
       </div>
@@ -51,23 +47,21 @@ const page = customElements.defineWithRender('home-page', class extends HTMLElem
   }
 
   stateSelectChange(value) {
-    console.log('stateSelectChange', value);
     this.selectedState = value;
     const state = this.states.find(i => i.name === value);
     if (state) this.cities = state.cities;
     else this.cities = [];
+    this.selectedCity = null;
     this.render();
   }
 
   citySelectChange(value) {
-    console.log('stateSelectChange', value);
     this.selectedCity = value;
   }
 });
 
 module.exports = async () => {
-  const states = getStates();
-
+  const states = await getStates();
   return {
     title: 'Interactive',
     body: page.template({ states })
