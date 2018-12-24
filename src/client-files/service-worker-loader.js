4e3213c06da36cc76b86297896b80cab6a635d34
Ben Rubin <ben@benjaminrubin.com>
2018-12-24 12:08:59 -0600

    fix syntax error new version

--- src/client-files/service-worker-loader.js
+++ src/client-files/service-worker-loader.js
@@ -1,6 +1,6 @@
 module.exports = params => `
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('${params.host}/service-worker.js'})
+    navigator.serviceWorker.register('${params.host}/service-worker.js')
       .then(function(registration) {
         console.log('Registration successful, scope is:', registration.scope);
       })
