f9764d044893cb65b9cf54c98202974f412d28f5
Ben Rubin <ben@benjaminrubin.com>
2018-12-24 16:06:55 -0600

    remove dependency new version

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const customElements = require('./src/customElements');
 const HTMLElement = require('./src/HTMLElement');
 const PageMapper = require('./src/PageMapper');
-const inject = require('./src/inject');
 const fileHandler = require('./src/fileHandler');
 const { registerComponent } = require('./src/componentRegistry');
 const { set } = require('./src/config');
